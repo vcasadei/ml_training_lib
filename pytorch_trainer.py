b6a887b075d13627e213e227235e5cbc88d56ddf
Vitor Casadei <vc@cesar.org.br>
2019-01-26 08:37:19 -0400

    teste

--- pytorch_trainer.py
+++ pytorch_trainer.py
@@ -110,7 +110,7 @@ class DeepNetTrainer(object):
 
                     Ypred, loss = self._do_optimize(X, Y)
 
-                    vloss = loss.data
+                    vloss = loss.data.cpu[0]
                     if hasattr(self.criterion, 'size_average') and self.criterion.size_average:
                         epo_loss += mb_size * vloss
                     else:
@@ -147,7 +147,7 @@ class DeepNetTrainer(object):
 
                         Ypred, loss = self._do_evaluate(X, Y)
 
-                        vloss = loss.data.cpu().detach().numpy()
+                        vloss = loss.data.cpu()[0]
                         if hasattr(self.criterion, 'size_average') and self.criterion.size_average:
                             epo_loss += vloss * mb_size
                         else:
@@ -199,7 +199,7 @@ class DeepNetTrainer(object):
 
                 Ypred, loss = self._do_evaluate(X, Y)
 
-                vloss = loss.data.cpu().detach().numpy()
+                vloss = loss.data.cpu()[0]
                 if hasattr(self.criterion, 'size_average') and self.criterion.size_average:
                     epo_loss += vloss * mb_size
                 else:
